675f5bb582fde9753db5111a46b67f5164580e13
Damir Mustafin <frenchthebread@gmail.com>
2015-07-27 00:30:34 +0300

    dynamic select box

---
+++ public/js/toggleSelect.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('#service').on('change', function() {
+    switch (this.value) {
+      case "transfer":
+          $('#dynamicPlace').html($('#hiddenTransfer').html());
+        break;
+      case "withDriver":
+          $('#dynamicPlace').html($('#hiddenWithDriver').html());
+        break;
+      case "excursion":
+          $('#dynamicPlace').html($('#hiddenExcursion').html());
+        break;
+      case "food":
+          $('#dynamicPlace').html($('#hiddenFood').html());
+        break;
+    }
+  });
+});
