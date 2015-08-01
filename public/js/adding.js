dfd52e53599dc53b677d54150d1020d93b669d3d
Damir Mustafin <frenchthebread@gmail.com>
2015-08-01 20:34:05 +0300

    added multiple accommodations adding

--- public/js/adding.js
+++ public/js/adding.js
@@ -1,52 +1,76 @@
 $(document).ready(function () {
 
-    var ids = [];
-    var rooms = $('#rooms');
-    var id = $('#rooms>.roomForm').size();
+    var roomIds = [];
+    var rooms   = $('#rooms');
+    var roomId  = $('#rooms>.roomForm').size();
 
-    ids.push(id);
+    var accommodationIds    = [];
+    var accommodations      = $('#accommodations');
+    var accommodationId     = $('#accommodations>.accommodationForm').size();
+
+    roomIds.push(roomId);
+    accommodationIds.push(accommodationId);
 
     addRoom = function () {
 
-        id++;
+        roomId++;
 
         // TODO: fill up options for room types dynamically
         var form = [
             '<div class="roomForm">' +
-                '<div id="roomType" class="form-group">' +
-                    '<label for="roomType_'+id+'">Тип комнаты</label><br/>' +
-                    '<select id="roomType_'+id+'" name="roomType_'+id+'" class="form-control">' +
+                '<div id="roomType_"'+roomId+' class="form-group">' +
+                    '<label for="roomType_'+roomId+'">Тип комнаты</label><br/>' +
+                    '<select id="roomType_'+roomId+'" name="roomType_'+roomId+'" class="form-control">' +
                         '<option>Тип комнаты</option>' +
                     '</select>' +
                 '</div>' +
-                '<div id="roomAmount_'+id+'" class="form-group">' +
+                '<div id="roomAmount_'+roomId+'" class="form-group">' +
                     '<label for="roomsAmount">Кол-во номеров</label><br/>' +
-                    '<input id="roomsAmount_'+id+'" type="text" placeholder="Кол-во номеров" name="roomAmount+'+id+'" class="form-control"/>' +
+                    '<input id="roomsAmount_'+roomId+'" type="text" placeholder="Кол-во номеров" name="roomAmount+'+roomId+'" class="form-control"/>' +
                 '</div>' +
                 '<div class="form-group" style="padding:2px;padding-top:30px;">' +
-                    '<a href="#" class="btn btn-xs btn-danger" onclick="removeRoom('+id+')">x</a>' +
+                    '<a href="#" class="btn btn-xs btn-danger" onclick="removeRoom('+roomId+')">x</a>' +
                 '</div>' +
             '</div>'
         ].join();
 
         $(form).appendTo(rooms);
-
-        ids.push(id);
+        roomIds.push(roomId);
 
         return false;
     };
 
-    removeRoom  = function (idd) {
+    removeRoom  = function (RoomIdd) {
 
-        $('#roomType_'+idd).closest('.roomForm').remove();
-
-        var index = ids.indexOf(idd);
-
-        var inList = index > -1;
-
-        if(inList) ids.splice(index, 1);
+        $('#roomType_'+RoomIdd).closest('.roomForm').remove();
+        var index   = roomIds.indexOf(RoomIdd);
+        var inList  = index > -1;
+        if (inList) roomIds.splice(index, 1);
 
         return false;
-    }
+    };
 
-})
+    addAccommodation = function () {
+
+        accommodationId++;
+
+        var form = '<div class="alert-danger row accommodationForm"><div id="accommodationField_'+accommodationId+'">Hello</div> '+ accommodationId +' <a href="#" class="btn btn-xs btn-danger" onclick="removeAccommodation('+accommodationId+')">x</a></div>';
+
+        $(form).appendTo(accommodations);
+        accommodationIds.push(accommodationId);
+
+        return false;
+    };
+
+    removeAccommodation = function (accommodationIdd) {
+
+        $('#accommodationField_'+accommodationIdd).closest('.accommodationForm').remove();
+
+        var index   = accommodationIds.indexOf(accommodationIdd);
+        var inList  = index > -1;
+        if (inList) accommodationIds.splice(index, 1);
+
+        return false;
+    };
+
+});
