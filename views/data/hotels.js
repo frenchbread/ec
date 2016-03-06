b69e845b98d0c8da90c66e010d76b07fea769369
Damir Mustafin <frenchthebread@gmail.com>
2016-03-07 00:58:31 +0200

    Hotel data, options & rooms

---
+++ views/data/hotels.js
@@ -0,0 +1,242 @@
+module.exports = [
+    {
+        codeName: "grand",
+        name: "СПБ - Grand Hotel Europe (5 stars)",
+        dateIn: new Date(2016, 1, 1),
+        dateOut: new Date(2016, 12, 31),
+        roomType: {
+            single: {
+                eur: 0,
+                rub: 4000
+            },
+            double: {
+                eur: 0,
+                rub: 5000
+            },
+            tripple: {
+                eur: 0,
+                rub: 6000
+            }
+        },
+        extraBed: {
+            eur: 0,
+            rub: 1000
+        }
+    },
+    {
+        codeName: "w",
+        name: "СПБ - W Hotel (5 stars)",
+        dateIn: new Date(2016, 1, 1),
+        dateOut: new Date(2016, 12, 31),
+        roomType: {
+            single: {
+                eur: 0,
+                rub: 4500
+            },
+            double: {
+                eur: 0,
+                rub: 5500
+            },
+            tripple: {
+                eur: 0,
+                rub: 6500
+            }
+        },
+        extraBed: {
+            eur: 0,
+            rub: 1000
+        }
+    },
+    {
+        codeName: "astoria",
+        name: "СПБ - Astoria (5 stars)",
+        dateIn: new Date(2016, 1, 1),
+        dateOut: new Date(2016, 12, 31),
+        roomType: {
+            single: {
+                eur: 0,
+                rub: 3000
+            },
+            double: {
+                eur: 0,
+                rub: 4000
+            },
+            tripple: {
+                eur: 0,
+                rub: 5000
+            }
+        },
+        extraBed: {
+            eur: 0,
+            rub: 1000
+        }
+    },
+    {
+        codeName: "agnle",
+        name: "СПБ - Angleterre (4 stars)",
+        dateIn: new Date(2016, 1, 1),
+        dateOut: new Date(2016, 12, 31),
+        roomType: {
+            single: {
+                eur: 0,
+                rub: 2000
+            },
+            double: {
+                eur: 0,
+                rub: 3000
+            },
+            tripple: {
+                eur: 0,
+                rub: 4000
+            }
+        },
+        extraBed: {
+            eur: 0,
+            rub: 1000
+        }
+    },
+    {
+        codeName: "crowne",
+        name: "СПБ - CROWNE PLAZA LIGOVSKY (4 stars)",
+        dateIn: new Date(2016, 1, 1),
+        dateOut: new Date(2016, 12, 31),
+        roomType: {
+            single: {
+                eur: 0,
+                rub: 3500
+            },
+            double: {
+                eur: 0,
+                rub: 4500
+            },
+            tripple: {
+                eur: 0,
+                rub: 5500
+            }
+        },
+        extraBed: {
+            eur: 0,
+            rub: 1000
+        }
+    },
+    {
+        codeName: "dostoevsky",
+        name: "СПБ - Dostoevsky (3 stars)",
+        dateIn: new Date(2016, 1, 1),
+        dateOut: new Date(2016, 12, 31),
+        roomType: {
+            single: {
+                eur: 0,
+                rub: 1500
+            },
+            double: {
+                eur: 0,
+                rub: 2500
+            },
+            tripple: {
+                eur: 0,
+                rub: 3500
+            }
+        },
+        extraBed: {
+            eur: 0,
+            rub: 1000
+        }
+    },
+    {
+        codeName: "moika",
+        name: "СПБ - MOIKA 22 Kempinski (5 stars)",
+        dateIn: new Date(2016, 1, 1),
+        dateOut: new Date(2016, 12, 31),
+        roomType: {
+            single: {
+                eur: 0,
+                rub: 4500
+            },
+            double: {
+                eur: 0,
+                rub: 5500
+            },
+            tripple: {
+                eur: 0,
+                rub: 6500
+            }
+        },
+        extraBed: {
+            eur: 0,
+            rub: 1000
+        }
+    },
+    {
+        codeName: "courtyard",
+        name: "СПБ - COURTYARD by Marriott Vasilievsky",
+        dateIn: new Date(2016, 1, 1),
+        dateOut: new Date(2016, 12, 31),
+        roomType: {
+            single: {
+                eur: 0,
+                rub: 2000
+            },
+            double: {
+                eur: 0,
+                rub: 3000
+            },
+            tripple: {
+                eur: 0,
+                rub: 4500
+            }
+        },
+        extraBed: {
+            eur: 0,
+            rub: 1000
+        }
+    },
+    {
+        codeName: "sokosPalace",
+        name: "СПБ - SOKOS PALACE BRIDGE (5 stars)",
+        dateIn: new Date(2016, 1, 1),
+        dateOut: new Date(2016, 12, 31),
+        roomType: {
+            single: {
+                eur: 0,
+                rub: 4500
+            },
+            double: {
+                eur: 0,
+                rub: 5500
+            },
+            tripple: {
+                eur: 0,
+                rub: 6000
+            }
+        },
+        extraBed: {
+            eur: 0,
+            rub: 1000
+        }
+    },
+    {
+        codeName: "sokosOlimpia",
+        name: "СПБ - SOKOS Olimpia Garden (5 stars)",
+        dateIn: new Date(2016, 1, 1),
+        dateOut: new Date(2016, 12, 31),
+        roomType: {
+            single: {
+                eur: 0,
+                rub: 4500
+            },
+            double: {
+                eur: 0,
+                rub: 5500
+            },
+            tripple: {
+                eur: 0,
+                rub: 6000
+            }
+        },
+        extraBed: {
+            eur: 0,
+            rub: 1000
+        }
+    }
+];
---
+++ views/data/roomTypes.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        title: "Одиночная",
+        value: "single"
+    },
+    {
+        title: "Двойная",
+        value: "double"
+    },
+    {
+        title: "Тройная",
+        value: "triple"
+    }
+];
