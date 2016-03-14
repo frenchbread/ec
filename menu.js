2d019a381fffd003ff979c74e5142b7d05a41a2f
Damir Mustafin <frenchthebread@gmail.com>
2016-03-14 06:25:47 +0200

    Menu template

---
+++ menu.js
@@ -0,0 +1,59 @@
+const remote = require('electron').remote;
+const ipc = require('electron').ipcRenderer;
+const Menu = remote.Menu;
+
+var template = [
+  {
+    label: 'Calc',
+    submenu: [
+      {
+        label: 'Show databases',
+        click: function () {
+          ipc.send('open-db');
+        }
+      }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.reload();
+        }
+      },
+      {
+        label: 'Toggle Full Screen',
+        accelerator: (function() {
+          if (process.platform == 'darwin')
+            return 'Ctrl+Command+F';
+          else
+            return 'F11';
+        })(),
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+        }
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: (function() {
+          if (process.platform == 'darwin')
+            return 'Alt+Command+I';
+          else
+            return 'Ctrl+Shift+I';
+        })(),
+        click: function(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.toggleDevTools();
+        }
+      },
+    ]
+  },
+];
+
+var menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
