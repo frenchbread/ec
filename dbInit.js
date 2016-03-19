9508aa63880e08035739a7a0333ac02bc359d16b
Damir Mustafin <frenchthebread@gmail.com>
2016-03-19 04:21:09 +0200

    Drivers & transfers

--- dbInit.js
+++ dbInit.js
@@ -1,7 +1,12 @@
 var Datastore = require('nedb');
 
-var restaurants = new Datastore({ filename: './db/restaurants.db', autoload: true });
 var hotels = new Datastore({ filename: './db/hotels.db', autoload: true });
+var transfers = new Datastore({ filename: './db/transfers.db', autoload: true });
+var drivers = new Datastore({ filename: './db/drivers.db', autoload: true });
 
-exports.restaurants = restaurants
+var restaurants = new Datastore({ filename: './db/restaurants.db', autoload: true });
+
 exports.hotels = hotels;
+exports.transfers = transfers;
+exports.drivers = drivers;
+exports.restaurants = restaurants;
