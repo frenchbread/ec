9bfde20e23c7c66428d0b09633a0be3a7d995242
Damir Mustafin <frenchthebread@gmail.com>
2016-03-23 02:10:49 +0200

    Excursions added to main page

--- dbInit.js
+++ dbInit.js
@@ -3,10 +3,11 @@ var Datastore = require('nedb');
 var hotels = new Datastore({ filename: './db/hotels.db', autoload: true });
 var transfers = new Datastore({ filename: './db/transfers.db', autoload: true });
 var drivers = new Datastore({ filename: './db/drivers.db', autoload: true });
-
+var excursions = new Datastore({ filename: './db/excursions.db', autoload: true });
 var restaurants = new Datastore({ filename: './db/restaurants.db', autoload: true });
 
 exports.hotels = hotels;
 exports.transfers = transfers;
 exports.drivers = drivers;
+exports.excursions = excursions;
 exports.restaurants = restaurants;
