134fdb9d41bbd3ee4933572d8b9e085eab49d40c
Damir Mustafin <frenchthebread@gmail.com>
2016-03-24 01:52:51 +0200

    path fix

--- dbInit.js
+++ dbInit.js
@@ -1,10 +1,10 @@
 var Datastore = require('nedb');
 
-var hotels = new Datastore({ filename: './db/hotels.db', autoload: true });
-var transfers = new Datastore({ filename: './db/transfers.db', autoload: true });
-var drivers = new Datastore({ filename: './db/drivers.db', autoload: true });
-var excursions = new Datastore({ filename: './db/excursions.db', autoload: true });
-var restaurants = new Datastore({ filename: './db/restaurants.db', autoload: true });
+var hotels = new Datastore({ filename: __dirname + '/db/hotels.db', autoload: true });
+var transfers = new Datastore({ filename: __dirname + '/db/transfers.db', autoload: true });
+var drivers = new Datastore({ filename: __dirname + '/db/drivers.db', autoload: true });
+var excursions = new Datastore({ filename: __dirname + '/db/excursions.db', autoload: true });
+var restaurants = new Datastore({ filename: __dirname + '/db/restaurants.db', autoload: true });
 
 exports.hotels = hotels;
 exports.transfers = transfers;
